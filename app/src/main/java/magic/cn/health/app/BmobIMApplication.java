767e969c05c36324fc42f0e35258e107acb419bc
Linsixu <386479427@qq.com>
2018-03-08 12:33:47 +0800

    第一次提交

---
+++ app/src/main/java/magic/cn/health/app/BmobIMApplication.java
@@ -0,0 +1,44 @@
+package magic.cn.health.app;
+
+import android.app.Application;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+import cn.bmob.newim.BmobIM;
+
+/**
+ * @author 林思旭
+ * @since 2018/3/8
+ */
+
+public class BmobIMApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //TODO 集成：1.8、初始化IM SDK，并注册消息接收器
+        if (getApplicationInfo().packageName.equals(getMyProcessName())){
+            BmobIM.init(this);
+//            BmobIM.registerDefaultMessageHandler(new DemoMessageHandler());
+        }
+    }
+
+    /**
+     * 获取当前运行的进程名
+     * @return
+     */
+    public static String getMyProcessName() {
+        try {
+            File file = new File("/proc/" + android.os.Process.myPid() + "/" + "cmdline");
+            BufferedReader mBufferedReader = new BufferedReader(new FileReader(file));
+            String processName = mBufferedReader.readLine().trim();
+            mBufferedReader.close();
+            return processName;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
